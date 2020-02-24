fc11775bf0e8e1df5b17f8dd3ce719e90734a80a
hongweiy <hongweiy@arfront.com>
2020-02-24 21:20:25 +0800

    commit install sql and make config file

---
+++ install-sql.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+HOSTNAME='$DATABASE_HOST' #数据库信息
+
+PORT='$DATABASE_PORT'
+
+USERNAME='$DATABASE_USERE'
+PASSWORD='$DATABASE_PWD'
+
+DBNAME='$DATABASE_NAME'  #数据库名称
+
+ADMINUSER = '$ADMIN_USE'
+ADMINPASSWORD = '$RADMIN_PWD'
+
+
+
+
+
+
+
+
+
+#创建数据库
+
+create_db_sql="create database IF NOT EXISTS ${DBNAME}"
+
+mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} -e "${create_db_sql}"
+mysql -u ${USERNAME} -p${PASSWORD} ${DBNAME} < /private/var/www/html/YouPHPTube/install/database.sql
+
+sign=`echo -n  $ADMINPASSWORD | md5sum`
+sign=`echo -n $ADMINPASSWORD |md5sum|cut -d ' ' -f1`
+
+
+#插入初始化管理员账号
+insert_admin_sql="INSERT INTO users (id, user, email, password, created, modified, isAdmin) VALUES (1, '{$ADMINUSER}', '90333@qq.com', '${sign}', now(), now(), true)"
+mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${insert_admin_sql}"
+
+#删除默认分类
+delete_category_sql="DELETE FROM categories WHERE id = 1 "
+mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${delete_category_sql}"
+
+#插入默认分类值
+insert_sql = "INSERT INTO categories (id, name, clean_name, description, created, modified) VALUES (1, 'Default', 'default','', now(), now())"
+
+#删除默认配置
+delete_config_sql = "DELETE FROM configurations WHERE id = 1 ";
+mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${delete_config_sql}"
+
+#插入默认配置
+insert_congig_sql =  "INSERT INTO configurations (id, video_resolution, users_id, version, webSiteTitle, language, contactEmail, encoderURL,  created, modified) VALUES (1, '858:480', 1,'7.8', '{$WEB_TITLE}', 'en', '907262317@qq.com', '{$WEB_URL}', now(), now())";
+
+
+mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${insert_congig_sql}"
---
+++ make-settings.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "<?php
+\$global['configurationVersion'] = 2;
+\$global['disableAdvancedConfigurations'] = 0;
+\$global['videoStorageLimitMinutes'] = 0;
+if(!empty(\$_SERVER['SERVER_NAME']) && \$_SERVER['SERVER_NAME']!=='localhost' && !filter_var(\$_SERVER['SERVER_NAME'], FILTER_VALIDATE_IP)) {
+    // get the subdirectory, if exists
+    \$subDir = str_replace(array(\$_SERVER[\"DOCUMENT_ROOT\"], 'videos/configuration.php'), array('',''), __FILE__);
+    \$global['webSiteRootURL'] = \"http\".(!empty(\$_SERVER['HTTPS'])?\"s\":\"\").\"://\".\$_SERVER['SERVER_NAME'].\$subDir;
+}else{
+    \$global['webSiteRootURL'] = '$WEB_URL';
+}
+\$global['systemRootPath'] = '/var/www/html/';
+\$global['salt'] = uniqid();
+\$global['enableDDOSprotection'] = 1;
+\$global['ddosMaxConnections'] = 40;
+\$global['ddosSecondTimeout'] = 5;
+\$global['strictDDOSprotection'] = 0;
+
+\$mysqlHost = '$DATABASE_HOST';
+\$mysqlPort = '$DATABASE_PORT';
+\$mysqlUser = '$DATABASE_USERE';
+\$mysqlPass = '$DATABASE_PWD';
+\$mysqlDatabase = '$DATABASE_NAME';
+
+/**
+ * Do NOT change from here
+ */
+
+require_once \$global['systemRootPath'].'objects/include_config.php';" > /private/var/www/html/YouPHPTube/videos/configuration.php
+
+exec "$@"
