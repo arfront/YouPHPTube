5cdb8401653790ae43b001e912ecdb334686cf16
hongweiy <hongweiy@arfront.com>
2020-03-04 15:40:15 +0800

    add S3 plugin

--- install-sql.sh
+++ install-sql.sh
@@ -10,6 +10,9 @@ DBNAME=$DATABASE_NAME  #数据库名称
 
 ADMINUSER=$ADMIN_USER
 ADMINPASSWORD=$ADMIN_PWD
+WEBTITLE=$WEB_TITLE #网站title
+WEBURL=$WEB_URL #网站域名
+
 
 
 
@@ -31,7 +34,7 @@ sign=`echo -n $ADMINPASSWORD |md5sum|cut -d ' ' -f1`
 
 
 #插入初始化管理员账号
-insert_admin_sql="INSERT INTO users (id, user, email, password, created, modified, isAdmin) VALUES (1, '{$ADMIN_USER}', '90333@qq.com', '${sign}', now(), now(), true)"
+insert_admin_sql="INSERT INTO users (id, user, email, password, created, modified, isAdmin) VALUES (1, '${ADMINUSER}', '90333@qq.com', '${sign}', now(), now(), true)"
 mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${insert_admin_sql}"
 
 #删除默认分类
@@ -46,7 +49,13 @@ delete_config_sql="DELETE FROM configurations WHERE id = 1";
 mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${delete_config_sql}"
 
 #插入默认配置
-insert_congig_sql="INSERT INTO configurations (id, video_resolution, users_id, version, webSiteTitle, language, contactEmail, encoderURL,  created, modified) VALUES (1, '858:480', 1,'7.8', '{$WEB_TITLE}', 'en', '907262317@qq.com', '{$WEB_URL}', now(), now())";
+insert_congig_sql="INSERT INTO configurations (id, video_resolution, users_id, version, webSiteTitle, language, contactEmail, encoderURL,  created, modified) VALUES (1, '858:480', 1,'7.8', '${WEBTITLE}', 'en', '907262317@qq.com', '${WEBURL}', now(), now())"
 
 
 mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${insert_congig_sql}"
+
+#插入并S3插件配置
+
+insert_sthree_sql="INSERT INTO plugins (id, uuid, status, created, modified, object_data, name, dirName, pluginversion) VALUES (NULL, '1ddecbec-91db-4357-bb10-ee08b0913778', 'active', now(), now(), '{\"region\":\"cn-northwest-1\",\"bucket_name\":\"youphptube\",\"key\":\"AKIA6E2GEODUOVRFWGPT\",\"secret\":\"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\",\"endpoint\":\"\",\"profile\":\"\",\"useS3DirectLink\":true,\"presignedRequestSecondsTimeout\":\"43200\",\"CDN_Link\":\"\",\"makeMyFilesPublicRead\":false}', 'AWS_S3', 'AWS_S3', '1.0')"
+
+mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${insert_sthree_sql}"
